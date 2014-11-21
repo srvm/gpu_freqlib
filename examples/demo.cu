59d874299057ee6b53d2b29009f6ec3b0047ee57
Saurav Muralidharan <sauravm@cs.utah.edu>
2014-11-20 19:12:31 -0700

    Adding NVML init and shutdown code

--- examples/demo.cu
+++ examples/demo.cu
@@ -4,11 +4,10 @@
 #include <gpu_freqlib/freqlib.h>
 
 int main() {
-
   freqlib::instance knob;
 
-  std::vector<double> clock_frequencies{knob.get_supported_clocks()};
-  std::vector<double> mem_frequencies{knob.get_supported_mem_clocks()};
+  //std::vector<double> clock_frequencies{knob.get_supported_clocks()};
+  //std::vector<double> mem_frequencies{knob.get_supported_mem_clocks()};
 
   return 0;
 }
--- include/gpu_freqlib/freqlib.h
+++ include/gpu_freqlib/freqlib.h
@@ -1,10 +1,44 @@
 #pragma once
 
+#include <cstdio>
+#include <nvml.h>
+
+#define NVML_CALL(call)                                            \
+  do {                                                             \
+    nvmlReturn_t result = call;                                    \
+    if(NVML_SUCCESS != result) {                                   \
+      fprintf(stderr, "[Fatal] NVML call failed with error %s\n",  \
+              nvmlErrorString(result));                            \
+      result = nvmlShutdown();                                     \
+      if(NVML_SUCCESS != result) {                                 \
+        fprintf(stderr, "[Fatal] Failed to shutdown NVML: %s\n",   \
+                nvmlErrorString(result));                          \
+      }                                                            \
+    }                                                              \
+  } while(0)
+
 namespace freqlib {
   using freqlist_t = std::vector<double>;
+  using error_t = void;
+  using frequency_t = double;
 
   struct instance {
-    instance() {
+    instance() :
+      m_device(0) {
+      nvmlReturn_t result = nvmlInit();
+      if(NVML_SUCCESS != result) {
+        fprintf(stderr, "[Fatal] Failed to initialize NVML: %s\n",
+                nvmlErrorString(result));
+        //exit(-1);
+      }
+    }
+
+    ~instance() {
+      nvmlReturn_t result = nvmlShutdown();
+      if(NVML_SUCCESS != result) {
+        fprintf(stderr, "[Fatal] Failed to shutdown NVML: %s\n",
+                nvmlErrorString(result));
+      }
     }
 
     freqlist_t get_supported_clocks() {
@@ -12,6 +46,33 @@ namespace freqlib {
 
     freqlist_t get_supported_mem_clocks() {
     }
+
+    error_t step_up_clock() {
+    }
+
+    error_t step_down_clock() {
+    }
+
+    error_t step_up_mem_clock() {
+    }
+
+    error_t step_down_mem_clock() {
+    }
+
+    error_t set_clock(frequency_t value) {
+    }
+
+    error_t set_mem_clock(frequency_t value) {
+    }
+
+    frequency_t get_current_clock() {
+    }
+
+    frequency_t get_current_mem_clock() {
+    }
+
+  private:
+    int m_device;
   };
 
 } // ns freqlib
